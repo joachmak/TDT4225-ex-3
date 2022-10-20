b0cf14554fb84ef7b438c8980b81f49c38ade1a9
Jorgennk <jorgen_nk@live.no>
2022-10-20 10:58:13 +0200

    :adhesive_bandage: Fix for minor features not implemented correctly

--- task_1.py
+++ task_1.py
@@ -1,6 +1,34 @@
+import utils
+from classes.db import Db
+
+
+def create_coll(self, collection_name):
+    collection = self.db.create_collection(collection_name)    
+    print('Created collection: ', collection)
+
+
+def insert_users(db):
+    """ Inserts users based on directory structure """
+    all_users = utils.os.get_all_users()
+    labeled_users = utils.os.get_labeled_ids()
+    users_to_insert = list(map(lambda usr: (usr, usr in labeled_users), all_users))
+    print(users_to_insert)
+
+    """
+    utils.db.insert_rows(db,
+                         queries.INSERT_USER,
+                         users_to_insert,
+                         f"Inserting {len(users_to_insert)} users")
+    """
+
+
 
 def main():
-    print("Hello")
+    db = Db()
+    insert_users(db)
+
+
+    db.connection.close_connection()
 
 
 if __name__ == "__main__":
