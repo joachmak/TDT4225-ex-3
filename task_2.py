92eca5467234b50d6d29b5d9cffbc9af3ec5e2ff
joachmak <joachimmaksim@gmail.com>
2022-10-23 15:05:54 +0200

    feat: speed up task 9, include additional trackpoint data

--- task_2.py
+++ task_2.py
@@ -1,4 +1,6 @@
-from utils.constants import COLLECTION_USERS, COLLECTION_ACTIVITIES
+import datetime
+
+from utils.constants import COLLECTION_USERS, COLLECTION_ACTIVITIES, COLLECTION_TRACKPOINTS
 from utils.db_connector import DbConnector
 
 
@@ -25,11 +27,13 @@ def task_3(connection: DbConnector):
         }
     ]
     users = connection.db[COLLECTION_USERS].aggregate(pipeline)
+    print("Top 20 users (_id) with highest number of activities:")
     for i in range(20):
         print(f"{i + 1}. {users.next()}")
 
 
 def task_6(connection: DbConnector):
+    """ a) Find the task with the most activities. b) Is this also the year with the most recorded hours? """
     print_task_message(6)
     convert_start_and_end_date_to_date_obj = {
             "$project": {
@@ -50,7 +54,7 @@ def task_6(connection: DbConnector):
                 "$dateDiff": {
                     "startDate": "$start_time",
                     "endDate": "$end_time",
-                    "unit": "second"
+                    "unit": "second"  # we should use seconds, because if we used hours we would "lose data".
                 }
             }
         },
@@ -97,10 +101,77 @@ def task_6(connection: DbConnector):
           "(or in this case, seconds).")
 
 
+def task_9(connection: DbConnector):
+    """ Find all users who have invalid activities, and the number of invalid activities per user """
+    print_task_message(9)
+    # 1. Find all invalid activities by going through the trackpoint collection and finding "invalid trackpoints"
+    pipeline = [
+        {
+            "$project": {
+                "time": {
+                    "$toDate": "$time"
+                },
+                "previous_time": {
+                    "$toDate": "$previous_time"
+                },
+                "activity_id": "$activity_id",
+            },
+        },
+        {
+            "$project": {
+                "timediff": {
+                    "$dateDiff": {
+                        "startDate": "$previous_time",
+                        "endDate": "$time",
+                        "unit": "second"
+                    }
+                },
+                "activity_id": "$activity_id",
+            }
+        },
+        {
+            "$match": {"timediff": {"$gte": 300}}  # greater than or equal to 300 seconds (5 minutes)
+        },
+        {
+            "$group": {
+                "_id": "$activity_id",
+            },
+        },
+    ]
+    invalid_activities = connection.db[COLLECTION_TRACKPOINTS].aggregate(pipeline)
+    # 2. Generate a map of invalid activity_ids for fast lookup
+    invalid_activity_map = {}
+    for activity in invalid_activities:
+        invalid_activity_map[activity["_id"]] = 1
+    # 3. For each user, iterate through all activities and count number of invalid activities
+    users = connection.db[COLLECTION_USERS].find({})
+    user_invalid_activity_count = {}
+    for user in users:
+        _id = user["_id"]
+        activities = user["activity_ids"]
+        invalid_activity_count = 0
+        for activity in activities:
+            if activity in invalid_activity_map:
+                invalid_activity_count += 1
+        if invalid_activity_count > 0:
+            user_invalid_activity_count[_id] = invalid_activity_count
+    keys = list(user_invalid_activity_count.keys())
+    keys.sort()
+    print(f"There are {len(invalid_activity_map.keys())} invalid activities distributed among {len(keys)} users.")
+    print("User\tNumber of invalid activities")
+    print("-----------------------------------")
+    for key in keys:
+        print(f"{key}\t{user_invalid_activity_count[key]}")
+
+
+
+
+
 def main():
     conn: DbConnector = DbConnector()
     task_3(conn)
     task_6(conn)
+    task_9(conn)
     conn.close_connection()
 
 
